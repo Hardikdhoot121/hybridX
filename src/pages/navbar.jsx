f2dc58d50d85692b80331804c6a90d932df65c73
Coder-Garvit <mathurgarvti@gmail.com>
2025-12-15 05:59:51 +0530

    Just Made the Home Better only worked on frontend

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,10 +1,31 @@
-// this file contains the navbar layout with all the functional buttons
-import React from 'react'
+import React from "react";
 
-const navbar = () => {
+export default function Navbar() {
   return (
-    <div>navbar</div>
-  )
-}
+    <>
+      <div className="mx-auto mt-2 rounded-md flex items-center justify-center bg-[#3abbf5] text-white text-center font-display h-10 w-370">
+        Premium Test Series — Sale Ends Soon, Get it Now
+      </div>
 
-export default navbar
+      <nav className="flex items-center justify-center px-8 py-4 text-white font-display">
+        <img
+          className="rounded-1xl w-9 h-9"
+          src="../src/images/hybrid_logo.jpg"
+          alt="Hybrid Logo"
+        />
+        <ul className="flex space-x-7 ml-100 mr-100">
+          <li className="px-3 py-2 rounded-md hover:bg-white hover:text-blue-600 cursor-pointer transition-all duration-200">Home</li>
+          <li className="px-3 py-2 rounded-md hover:bg-white hover:text-blue-600 cursor-pointer transition-all duration-200">Tests</li>
+          <li className="px-3 py-2 rounded-md hover:bg-white hover:text-blue-600 cursor-pointer transition-all duration-200">Leaderboard</li>
+          <li className="px-3 py-2 rounded-md hover:bg-white hover:text-blue-600 cursor-pointer transition-all duration-200">Prices</li>
+          <li className="px-3 py-2 rounded-md hover:bg-white hover:text-blue-600 cursor-pointer transition-all duration-200">Contact Us</li>
+        </ul>
+        <img
+          className="rounded-1xl w-9 h-9"
+          src="/images/Avatar.png"
+          alt="Avatar"
+        />
+      </nav>
+    </>
+  );
+}
