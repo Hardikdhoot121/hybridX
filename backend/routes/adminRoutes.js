35430640f7bef50d56b3f41da7e5acc8f38f7837
Hardik <hardikdhoot121@gmail.com>
2026-06-15 00:06:18 +0530

    connect admin student routes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
 import {protect} from "../middlewares/authMiddleware.js";
 import { authorize } from "../middlewares/roleMiddleware.js";
+import adminStudentRoutes from "./adminStudentRoutes.js";
 
 const router = express.Router();
+
+router.use("/", protect, authorize("admin"), adminStudentRoutes);
 
 router.get(
   "/dashboard",
